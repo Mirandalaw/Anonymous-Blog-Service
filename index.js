5038ba859637eb902295df15652df97aa16f071e
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-02-02 01:33:22 +0900

    comment 추가

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { userRouter } from './apis/src/user/routes/userRoute.js';
+import { userRouter } from './apis/src/routes/userRoute.js';
 import dotenv from 'dotenv';
-import { blogRouter } from './apis/src/user/routes/blogRoute.js';
-
+import { blogRouter } from './apis/src/routes/blogRoute.js';
+import {commentRouter} from './apis/src/routes/commentRoute.js';
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.set("strictQuery", false);
-mongoose
-    .connect(MONGO_URI,{})
-    .then(()=> console.log('Success to connect DB'))
-    .catch(e => console.log(e.message));
+const server = async() =>{
+    try {
+        mongoose.set("strictQuery",false);
+        await mongoose.connect(MONGO_URI,{});
+        mongoose.set('debug',true);
+        app.use(express.json());
+        app.use(express.urlencoded({extended:false}));
+        app.use('/user',userRouter);
+        app.use('/blog',blogRouter);
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use('/user',userRouter);
-app.use('/blog',blogRouter);
+        app.listen(PORT,()=>{
+            console.log(`Express Server is listening at ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-app.listen(PORT,()=>{
-    console.log(`Express Server is listening at ${PORT}`);
-})
+server();
