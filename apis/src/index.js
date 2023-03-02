0adca8cce881c6f411d65ab6cb7f792108b829bd
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-03-02 19:24:40 +0900

    swagger yaml을 통해 path로 띄우기

--- apis/src/index.js
+++ apis/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { userRouter, blogRouter } = require('./routes');
-const { swaggerUi, specs } = require('./swagger/swagger');
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const yaml = require('yamljs');
 
+const app = express();
+const swaggerYaml = yaml.load(path.join(__dirname, './swagger/board-1.0.0.yaml'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerYaml));
 
 app.use('/user', userRouter);
 app.use('/blog', blogRouter);
